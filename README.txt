ebd9e1e36a5d4e427a836a6e35ad9342b737c538
helmut46 <helmut.sirait46@gmail.com>
2024-08-29 07:30:00 +0700

    add assets and index.html + readme.txt

---
+++ README.txt
@@ -0,0 +1,15 @@
+->**Personal Website Portfolio**<-
+Designed and creavitiy by helmut sirait
+
+Some of the code in certain parts of this website was not created by me, but I created the source from which the code was taken 
+
+Credits :
+menu burger : https://codepen.io/ainalem
+auto slide : https://codepen.io/shuho
+scroll to top : https://codepen.io/marcelrojas
+demo images : https://www.unsplash.com
+icons : https://simpleicons.org/
+image about : https://www.krea.ai/
+idea of design : https://www.pinterest.com/
+
+
---
+++ assets/js/script.js
@@ -0,0 +1,103 @@
+// burger menu
+const burgerMenu = document.querySelector('.burger-menu');
+const navMenu = document.querySelector('nav ul'); 
+const link = document.querySelectorAll('.links')
+const body = document.querySelector('body'); 
+burgerMenu.addEventListener('click', function(){
+      burgerMenu.classList.toggle('active');
+      navMenu.classList.toggle('active');
+      body.classList.toggle('active');
+
+      for(let i = 0; i < link.length; i++){
+             link[i].addEventListener('click', function() {
+                  body.removeAttribute('class');
+                  // navMenu.classList.toggle('active');
+                  navMenu.classList.remove('active');
+                  burgerMenu.classList.remove('active');
+             });
+      }
+     
+});
+
+
+// Slide Portfolio 
+let slidePortfolio = document.querySelectorAll('.portfolio-card');
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+let slideIndex = 0;
+
+ 
+prevButton.addEventListener('click', function(e){
+        const plusSlides = (n) => {
+            showSlides(slideIndex += n);
+        };
+        plusSlides(-1);
+});
+
+nextButton.addEventListener('click', function(){
+        const plusSlides = (n) => {
+            showSlides(slideIndex += n);
+        };
+        plusSlides(1);
+});
+
+const showSlides = (n) => {
+            let i;
+
+            if (n > slidePortfolio.length) {
+                slideIndex = 1;
+            }
+            if (n < 1) {
+                slideIndex = slidePortfolio.length;
+            }
+
+            for (i = 0; i < slidePortfolio.length; i++) {
+                slidePortfolio[i].style.display = "none";
+            }
+            slidePortfolio[slideIndex - 1].style.display = 'flex';
+        };
+
+    const autoSlides = function() {
+            let i;
+
+            for (i = 0; i < slidePortfolio.length; i++) {
+                slidePortfolio[i].style.display = "none";
+            }
+            slideIndex++;
+            if (slideIndex > slidePortfolio.length) {
+                slideIndex = 1;
+            }
+
+            slidePortfolio[slideIndex - 1].style.display = "flex";
+          
+        }
+        autoSlides();     
+
+
+// scroll to top
+ const scrollToTopBtn = document.querySelector(".toTop");
+ const rootElement = document.documentElement;
+
+function handleScroll() {
+  // Do something on scroll
+  const scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
+  if (rootElement.scrollTop / scrollTotal > .1) {
+    // Show button
+    scrollToTopBtn.classList.add("showBtn");
+  } else {
+    // Hide button
+    scrollToTopBtn.classList.remove("showBtn");
+  }
+}
+
+function scrollToTop() {
+  // Scroll to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
+scrollToTopBtn.addEventListener("click", scrollToTop);
+document.addEventListener("scroll", handleScroll); 
+
+
